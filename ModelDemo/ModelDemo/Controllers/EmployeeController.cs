ab079fee78897178b9f1e66ee6dbefb9b90236c6
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-18 18:09:45 +0530

    pushing new project

---
+++ ModelDemo/ModelDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelDemo.Models;
+namespace ModelDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       
+        // GET: Employee
+        public ActionResult Index()
+        {
+            EmpMaster empMaster = new EmpMaster {EmpCode=100,EmpName="Scott",Email="scott@gmail.com" };
+            return View(empMaster);
+        }
+
+        public ActionResult ShowEmployees()
+        {
+            List<EmpMaster> empList = new List<EmpMaster>
+            {
+            new EmpMaster{EmpCode=100,EmpName="Scott",Email="scott@gmail.com" },
+            new EmpMaster{EmpCode=101,EmpName="Tiger",Email="tiger@gmail.com" },
+            new EmpMaster{EmpCode=102,EmpName="Smith",Email="smith@gmail.com"}
+            };
+
+            return View(empList);
+        }
+    }
+}
---
+++ ModelDemo/ModelDemo/Models/EmpMaster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelDemo.Models
+{
+    public class EmpMaster
+    {
+        public int EmpCode { get; set; }
+        public string EmpName { get; set; }
+        public string Email { get; set; }
+    }
+}
